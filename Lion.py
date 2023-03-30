6509c290792d913d02803985f4a030767bc05b01
Md Noor <75603153+NoorXd786@users.noreply.github.com>
2023-03-30 22:52:06 +0530

    Update Lion.py

--- Lion.py
+++ Lion.py
@@ -6,7 +6,7 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 # Starting bot
-system("git clone https://github.com/Mdnoor786/LionX && cd LionX && python3 -m userbot")
+system("git clone https://github.com/NoorXd786/ShasaRobot && cd ShasaRobot && python3 -m Shasa")
 # if got error
 print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
 # Script written for stop heroku suspension
